f9330f85c69956615f149289db4ad8a55f20bfad
Shea Lewis <shea.lewis89@gmail.com>
2016-06-03 09:43:59 -0700

    Added methods to sync roles through permissions

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -28,4 +28,53 @@ class Permission extends Model
 	{
 		return $this->belongsToMany('\Caffeinated\Shinobi\Models\Role')->withTimestamps();
 	}
+
+	/**
+	 * Assigns the given role to the permission.
+	 *
+	 * @param  integer  $roleId
+	 * @return bool
+	 */
+	public function assignRole($roleId = null)
+	{
+		$roles = $this->roles;
+
+		if (! $roles->contains($roleId)) {
+			return $this->roles()->attach($roleId);
+		}
+
+		return false;
+	}
+
+	/**
+	 * Revokes the given role from the permission.
+	 *
+	 * @param  integer  $roleId
+	 * @return bool
+	 */
+	public function revokeRole($roleId = '')
+	{
+		return $this->roles()->detach($roleId);
+	}
+
+	/**
+	 * Syncs the given role(s) with the permission.
+	 *
+	 * @param  array  $roleIds
+	 * @return bool
+	 */
+	public function syncRoles(array $roleIds = array())
+	{
+		return $this->roles()->sync($roleIds);
+	}
+
+	/**
+	 * Revokes all roles from the permission.
+	 *
+	 * @return bool
+	 */
+	public function revokeAllRoles()
+	{
+		return $this->roles()->detach();
+	}
 }
